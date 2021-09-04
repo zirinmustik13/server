b7a888a7cac5b2956822da54c51a9cc2dc89a8b5
zirinmustik13 <90075635+zirinmustik13@users.noreply.github.com>
2021-09-04 03:07:56 +0000

    Update valid.sh

--- valid.sh
+++ valid.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.worke -p x --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RJqUqa4GeMWKJJ4c95roAQZJGgf1H2KsyW.zirin -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
